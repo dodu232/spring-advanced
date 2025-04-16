fb7b52412cbf06b764f279884473606ac33e46d7
dodu232 <dodu7510@gmail.com>
2025-04-16 19:56:06 +0900

    feat:  레벨 4: Interceptor를 활용한 로깅

---
+++ src/main/java/org/example/expert/config/LoggingInterceptor.java
@@ -0,0 +1,42 @@
+package org.example.expert.config;
+
+import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private final Logger logger;
+
+    public LoggingInterceptor(){
+        this.logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler, @Nullable ModelAndView modelAndView) {
+        String time = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            .format(LocalDateTime.now());
+        logger.info("Request Time: [{}] Method: [{}] URL: [{}] - Authentication succeeded", time,
+            request.getMethod(), request.getRequestURI());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+        Object object, @Nullable Exception ex) throws Exception {
+    }
+}
